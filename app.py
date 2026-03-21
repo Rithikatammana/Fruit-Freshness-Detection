e8b897399b6d7db2f71433bb8963ac025e5a3d13
Rithika Tammana <150833187+Rithikatammana@users.noreply.github.com>
2026-03-21 22:36:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+model = tf.keras.models.load_model("model/fruit_model.h5")
+
+# class labels
+class_names = [
+    "Fresh Apple",
+    "Fresh Banana",
+    "Fresh Orange",
+    "Rotten Apple",
+    "Rotten Banana",
+    "Rotten Orange"
+]
+
+st.title("🍎 Fruit Freshness Detection using AI")
+st.write("This application uses a Deep Learning model (MobileNetV2) to classify whether a fruit is Fresh or Rotten.")
+
+file = st.file_uploader("Upload Fruit Image", type=["jpg","png","jpeg"])
+
+if file:
+    image = Image.open(file).resize((224,224))
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = np.array(image)/255.0
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+    class_index = np.argmax(prediction)
+
+    result = class_names[class_index]
+
+    st.success(f"Prediction: {result}")
+    confidence = np.max(prediction)*100
+    st.write(f"Confidence: {confidence:.2f}%")
---
+++ train_model.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+
+train_dir = "dataset/train"
+test_dir = "dataset/test"
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(224,224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+base_model = MobileNetV2(weights='imagenet', include_top=False)
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128, activation='relu')(x)
+predictions = Dense(train_data.num_classes, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_data, epochs=10, validation_data=test_data)
+
+model.save("model/fruit_model.h5")
